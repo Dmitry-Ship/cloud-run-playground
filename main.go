ef3af0ac942de8d63179030bd3bd497de58f14c7
Dmitry-Ship <vooxil@gmail.com>
2021-05-06 19:43:02 +0200

    test

--- main.go
+++ main.go
@@ -17,7 +17,6 @@ func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error loading .env file")
 	}
-
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 	http.HandleFunc("/api", handleAPI)
 
